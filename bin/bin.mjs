411df31ffe40bae9b3b21fce5f4f7db6de003a38
Jascha <jascha@jaeh.at>
2020-01-07 21:50:11 +0100

    add bin test script

---
+++ bin/bin.mjs
@@ -0,0 +1,46 @@
+#!/usr/bin/env node
+
+import log from '@magic/log'
+
+import cli from '../src/index.mjs'
+
+const cliArgs = {
+  options: [
+    ['--name', '-n'],
+  ],
+  commands: ['hello'],
+  help: {
+    name: 'magic-cli hello world',
+    header: 'used to test the cli library.',
+    commands: {
+      hello: 'echo hello ${name}',
+    },
+    options: {
+      '--name': 'name to hello to',
+    },
+    example: `
+# the obligatory
+bin/bin.mjs hello --name World
+`,
+  },
+  default: {
+    '--name': 'World',
+  },
+  single: [
+    '--name',
+  ],
+  required: [
+    '--name'
+  ],
+}
+
+const { args, commands } = cli(cliArgs)
+
+if (commands.hello) {
+  let { name } = args
+  if (Array.isArray(name)) {
+    name = name[0]
+  }
+
+  log.success('hello,', name)
+}
