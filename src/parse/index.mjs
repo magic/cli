ef79fb3a7868fd087fdcb48f10b708dff664d36d
Jascha <jascha@jaeh.at>
2020-01-07 21:49:49 +0100

    parse required fields

--- src/parse/index.mjs
+++ src/parse/index.mjs
@@ -1,18 +1,27 @@
 import { parseEnv } from './env.mjs'
 import { parseArgv } from './argv.mjs'
 import { parseCommands } from './commands.mjs'
+import { parseRequired } from './required.mjs'
+import { clean } from './clean.mjs'
 
-export const parse = ({ pure = false, ...props }) => {
+export const parse = props => {
+  const { pure = false } = props
+
   const { pureEnv = pure, pureArgv = pure, pureCommands = pure } = props
 
   const env = parseEnv({ ...props, pure: pureEnv })
   const { argv, args } = parseArgv({ ...props, pure: pureArgv })
   const commands = parseCommands({ ...props, pure: pureCommands })
 
-  return {
+  const parsed = {
     env,
     argv,
     args,
     commands,
+    errors: [],
   }
+
+  parsed.errors = parseRequired(parsed)
+
+  return parsed
 }
