30f658cbc7e5eba7cc7d262160b4ad6865dc49fd
Jascha <jascha@jaeh.at>
2020-01-07 22:24:26 +0100

    clean both argv and args

--- src/parse/clean.mjs
+++ src/parse/clean.mjs
@@ -1,15 +1,22 @@
 import is from '@magic/types'
+import cases from '@magic/cases'
 
-export const clean = props => {
+export const clean = (cli, props) => {
   if (is.empty(props.single)) {
-    return props
+    return cli
   }
 
   props.single.map(s => {
-    if (props.options[s] && is.array(props.options[s])) {
-      props.options[s] = s.join(' ')
+    if (cli.argv[s] && is.array(cli.argv[s])) {
+      cli.argv[s] = cli.argv[s].join(' ')
+    }
+
+    const c = cases.camel(s)
+
+    if (cli.args[c] && is.array(cli.args[c])) {
+      cli.args[c] = cli.args[c].join(' ')
     }
   })
 
-  return props
+  return cli
 }
