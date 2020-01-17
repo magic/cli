64ffc261974b9ccb6c1033568d2db49a6631330e
Jascha <jascha@jaeh.at>
2020-01-17 22:09:34 +0100

    "cli all" now runs all defined commands

--- src/parse/commands.mjs
+++ src/parse/commands.mjs
@@ -1,26 +1,43 @@
 export const parseCommands = ({ commands = [], pure = false }) => {
+  const { argv } = process
+  const runAll = argv.includes('all')
+
   const cmds = {}
   commands.map((tasks = []) => {
     let key
     let idx = -1
-    if (typeof tasks === 'string' && process.argv.includes(tasks)) {
-      idx = process.argv.indexOf(tasks)
-      key = tasks
+    if (typeof tasks === 'string') {
+      if (argv.includes(tasks)) {
+        idx = argv.indexOf(tasks)
+        key = tasks
+      } else if (runAll) {
+        key = tasks
+      }
     } else {
       if (!Array.isArray(tasks)) {
         tasks = [tasks]
       }
-      const idxArray = tasks
-        .filter(task => process.argv.includes(task))
-        .map(task => process.argv.indexOf(task))
 
-      idx = idxArray[0]
-      key = tasks[0]
+      if (runAll) {
+        key = tasks[0]
+      } else {
+        const idxArray = tasks
+          .filter(task => argv.includes(task))
+          .map(task => argv.indexOf(task))
+
+        idx = idxArray[0]
+        key = tasks[0]
+      }
     }
 
-    if (!pure && idx > -1) {
+    if (idx > -1) {
       cmds[key] = true
-      process.argv[idx] = key
+
+      if (!pure) {
+        argv[idx] = key
+      }
+    } else {
+      cmds[key] = runAll
     }
   })
 
