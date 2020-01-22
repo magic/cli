2525e725c602f5b12df6828e845f9bca3e5f4a43
Jascha <jascha@jaeh.at>
2020-01-22 20:38:01 +0100

    add spec tests

--- test/spec.mjs
+++ test/spec.mjs
@@ -1,9 +1,11 @@
 import { is } from '@magic/test'
-import { cli } from '../src/index.mjs'
+import { cli, prompt, exec, spawn } from '../src/index.mjs'
 
 export default [
   { fn: () => cli, expect: is.function, info: 'cli exports a function' },
   { fn: () => cli.spawn, expect: is.function, info: 'cli.spawn exists' },
   { fn: () => cli.exec, expect: is.function, info: 'cli.exec exists' },
   { fn: () => cli.exec, expect: is.deep.equal(cli.spawn), info: 'cli.spawn equals cli.exec' },
+  { fn: () => cli.exec, expect: is.deep.equal(exec), info: 'cli.exec equals exec' },
+  { fn: () => cli.prompt, expect: is.deep.equal(prompt), info: 'cli.prompt equals prompt' },
 ]
