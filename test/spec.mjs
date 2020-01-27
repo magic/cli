4be49ec6f68bf45dd98648f0dce9a52ccc5b7937
jascha <jascha@jaeh.at>
2020-01-27 16:24:21 +0100

    update spec tests

--- test/spec.mjs
+++ test/spec.mjs
@@ -5,7 +5,8 @@ export default [
   { fn: () => cli, expect: is.function, info: 'cli exports a function' },
   { fn: () => cli.spawn, expect: is.function, info: 'cli.spawn exists' },
   { fn: () => cli.exec, expect: is.function, info: 'cli.exec exists' },
-  { fn: () => cli.exec, expect: is.deep.equal(cli.spawn), info: 'cli.spawn equals cli.exec' },
+  { fn: () => cli.prompt, expect: is.function, info: 'cli.exec exists' },
+  { fn: () => cli.spawn, expect: is.deep.equal(spawn), info: 'cli.spawn equals spawn' },
   { fn: () => cli.exec, expect: is.deep.equal(exec), info: 'cli.exec equals exec' },
   { fn: () => cli.prompt, expect: is.deep.equal(prompt), info: 'cli.prompt equals prompt' },
 ]
