c5b5ec934314a06ccf992207e748963435cb69c1
jascha <j@jaeh.at>
2021-12-22 13:31:03 +0100

    parse/commands: format

--- src/parse/commands.mjs
+++ src/parse/commands.mjs
@@ -6,40 +6,49 @@ export const parseCommands = (props = {}) => {
   const { argv } = process
   const runAll = argv.includes('all')
 
-  const cmds = Object.fromEntries(commands.map((tasks = []) => {
-    let key
-    let idx = -1
+  const cmds = Object.fromEntries(
+    commands
+      .map((tasks = []) => {
+        let key
+        let idx = -1
 
-    if (is.string(tasks)) {
-      if (argv.includes(tasks)) {
-        idx = argv.indexOf(tasks)
-        key = tasks
-      } else if (runAll) {
-        key = tasks
-      }
-    } else if (is.array(tasks)) {
-      if (runAll) {
-        key = tasks[0]
-      } else {
-        const idxArray = tasks.filter(task => argv.includes(task)).map(task => argv.indexOf(task))
+        if (is.string(tasks)) {
+          if (argv.includes(tasks)) {
+            idx = argv.indexOf(tasks)
+            key = tasks
+          } else if (runAll) {
+            key = tasks
+          }
+        } else if (is.array(tasks)) {
+          if (runAll) {
+            key = tasks[0]
+          } else {
+            const idxArray = tasks
+              .filter(task => argv.includes(task))
+              .map(task => argv.indexOf(task))
 
-        idx = idxArray[0]
-        key = tasks[0]
-      }
-    } else {
-      log.error('E_UNEXPECTED_TASK', `@magic/cli: got unexpected task type: ${typeof tasks}, can handle strings or arrays`)
-    }
+            idx = idxArray[0]
+            key = tasks[0]
+          }
+        } else {
+          log.error(
+            'E_UNEXPECTED_TASK',
+            `@magic/cli: got unexpected task type: ${typeof tasks}, can handle strings or arrays`,
+          )
+        }
 
-    if (idx > -1) {
-      if (!pure) {
-        argv[idx] = key
-      }
+        if (idx > -1) {
+          if (!pure) {
+            argv[idx] = key
+          }
 
-      return [ key, true ]
-    } else if (runAll) {
-      return [ key, true ]
-    }
-  }).filter(a => a))
+          return [key, true]
+        } else if (runAll) {
+          return [key, true]
+        }
+      })
+      .filter(a => a),
+  )
 
   return cmds
 }
