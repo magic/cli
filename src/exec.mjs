6f1ae28fd97a5f7b324050ca0150b233b2faa832
jascha <j@jaeh.at>
2021-12-22 13:08:51 +0100

    src/exec now uses @magic/error

--- src/exec.mjs
+++ src/exec.mjs
@@ -1,20 +1,20 @@
 import child_process from 'child_process'
 
+import error from '@magic/error'
+
 const libName = '@magic/cli.exec'
 
 export const exec = (cmd, options = {}) =>
   new Promise((resolve, reject) => {
     child_process.exec(cmd, options, (err, stdout, stderr) => {
       if (err) {
-        const e = new Error(`${libName}: ${cmd} error: ${err.message}`)
-        e.name = 'E_EXEC_ERR'
+        const e = error(err, 'E_EXEC_ERR')
         reject(e)
         return
       }
 
       if (stderr) {
-        const e = new Error(`${libName}: ${cmd} error: ${stderr}`)
-        e.name = 'E_EXEC_STDERR'
+        const e = error(new Error(`${libName}: ${cmd} error: ${stderr}`), 'E_EXEC_STDERR')
         reject(e)
         return
       }
