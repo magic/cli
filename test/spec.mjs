42607216147d9359fdf480d7e272a3e42d09d5ba
jascha <j@jaeh.at>
2021-12-24 23:12:11 +0100

    test/spec: use @magic/test.version

--- test/spec.mjs
+++ test/spec.mjs
@@ -1,12 +1,17 @@
-import { is } from '@magic/test'
-import { cli, prompt, exec, spawn } from '../src/index.mjs'
+import { is, version } from '@magic/test'
+import { cli, prompt, exec, execFile, spawn } from '../src/index.mjs'
+
+const spec = {
+  spawn: 'fn',
+  exec: 'fn',
+  execFile: 'fn',
+  prompt: 'fn',
+}
 
 export default [
-  { fn: () => cli, expect: is.function, info: 'cli exports a function' },
-  { fn: () => cli.spawn, expect: is.function, info: 'cli.spawn exists' },
-  { fn: () => cli.exec, expect: is.function, info: 'cli.exec exists' },
-  { fn: () => cli.prompt, expect: is.function, info: 'cli.exec exists' },
+  ...version(cli, spec),
   { fn: () => cli.spawn, expect: is.deep.equal(spawn), info: 'cli.spawn equals spawn' },
   { fn: () => cli.exec, expect: is.deep.equal(exec), info: 'cli.exec equals exec' },
+  { fn: () => cli.execFile, expect: is.deep.equal(execFile), info: 'cli.exec equals exec' },
   { fn: () => cli.prompt, expect: is.deep.equal(prompt), info: 'cli.prompt equals prompt' },
 ]
